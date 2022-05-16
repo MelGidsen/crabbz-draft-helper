0a3033e16aa6b8608d32ce59653f2539b6d6cfe0
Gideon Gaida <26778393+MelGidsen@users.noreply.github.com>
2022-05-16 23:36:50 +0200

    adds champ icon adds champ interfaces + champs up to anivia

---
+++ src/app/core/interfaces/champions.interface.ts
@@ -0,0 +1,82 @@
+namespace IChampion {
+  export type Class =
+    | 'ASS'
+    | 'SKI'
+    | 'DIV'
+    | 'JUG'
+    | 'VAN'
+    | 'WAR'
+    | 'CAT'
+    | 'ENC'
+    | 'ART'
+    | 'BUR'
+    | 'BAT'
+    | 'SPE'
+    | 'MAR';
+
+  export type Position = 'TOP' | 'JUN' | 'MID' | 'ADC' | 'SUP';
+  export type Range = 'MEELE' | 'SHORT' | 'LONG';
+  export type DamageProfile = 'PHYSICAL' | 'MAGIC' | 'HYBRID' | 'UTIL';
+
+  export type ContributionProfile =
+    | 'CC_AOE'
+    | 'CC_IMPACT'
+    | 'CC_SINGLE_TARGET'
+    | 'CC_RANGE'
+    | 'SUSTAIN'
+    | 'UTILITY'
+    | 'ENGAGE'
+    | 'PEEL'
+    | 'DMG_SINGLE_TARGET'
+    | 'DMG_AOE'
+    | 'DMG_DPS'
+    | 'BURST'
+    | 'POKE'
+    | 'MITIGATION'
+    | 'REPOSITION'
+    | 'SKIRMISH'
+    | 'SPLIT'
+    | 'SIEGE'
+    | 'ZONE_CONTROL'
+    | 'WAVE_CLEAR';
+
+  export type Playstyle = 'SPLIT' | 'PROT' | 'PICK' | 'ENGAGE' | 'SIEGE';
+
+  export type InvadeProfile = 'SAFE' | 'FOLLOW' | 'INVADE' | 'COUNTER';
+
+  export interface Invade {
+    type: InvadeProfile;
+    name: string;
+    text: string;
+    color: string;
+  }
+
+  export interface Champion {
+    id: string;
+    name: string;
+    class: Class[];
+    icon: string;
+    positions: Position[];
+    range: Range;
+    damage: Damage;
+    contribution: Contribution;
+    scaling: Scaling;
+    invade: Invade[];
+    playstyle: Playstyle[];
+  }
+
+  export interface Damage {
+    type: DamageProfile;
+    amount: number;
+  }
+
+  export type Contribution = {
+    [key in ContributionProfile]?: number;
+  };
+
+  export interface Scaling {
+    early: number;
+    mid: number;
+    late: number;
+  }
+}
